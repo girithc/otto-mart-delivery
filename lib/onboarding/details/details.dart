e8b5810f9e2bdb4f3e458d931cb8eeab797eca11
girithc <girithmchoudhary@gmail.com>
2024-01-03 23:34:42 +0530

    translate to hindi

--- lib/onboarding/details/details.dart
+++ lib/onboarding/details/details.dart
@@ -10,10 +10,34 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  bool isEnglish = true;
   final ImagePicker _picker = ImagePicker();
   XFile? aadharCardImage;
   XFile? panCardImage;
   XFile? personalPhoto;
+
+  void toggleLanguage() {
+    setState(() {
+      isEnglish = !isEnglish;
+    });
+  }
+
+  String translateToHindi(String text) {
+    var translations = {
+      'Welcome To Otto Mart': 'ओटो मार्ट में आपका स्वागत है',
+      'Personal Photo': 'व्यक्तिगत फोटो',
+      'Aadhar Card': 'आधार कार्ड',
+      'PAN Card': 'पैन कार्ड',
+      'Name': 'नाम',
+      'Age': 'उम्र',
+      'Enter Address': 'पता दर्ज करें',
+      'Aadhar Card Number': 'आधार कार्ड संख्या',
+      'PAN Card Number': 'पैन कार्ड संख्या',
+      'Upload Image': 'छवि अपलोड करें',
+      'Click a Selfie': 'सेल्फी लें'
+    };
+    return translations[text] ?? text;
+  }
 
   Future<void> _pickImage(ImageSource source, String type) async {
     final pickedFile = await _picker.pickImage(source: source);
@@ -34,7 +58,15 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome To Otto Mart'),
+        title: Text(isEnglish
+            ? 'Welcome To Otto Mart'
+            : translateToHindi('Welcome To Otto Mart')),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.language),
+            onPressed: toggleLanguage,
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -42,105 +74,15 @@ class _DetailsPageState extends State<DetailsPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                padding: EdgeInsets.all(10), // Adds horizontal margin
-                decoration: BoxDecoration(
-                  color: Colors.white, // Set the background color
-                  borderRadius: BorderRadius.circular(
-                      10), // Optional: Adds rounded corners
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(
-                          0.5), // Shadow color with some transparency
-                      spreadRadius: 2,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // Changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Name',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Age',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    _imageField('Personal Photo', personalPhoto,
-                        cameraOnly: true),
-                  ],
-                ),
-              ),
+              _buildDetailsContainer('Name', 'Age', 'Enter Address',
+                  'Personal Photo', personalPhoto,
+                  cameraOnly: true),
               const SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.all(10), // Adds horizontal margin
-                decoration: BoxDecoration(
-                  color: Colors.white, // Set the background color
-                  borderRadius: BorderRadius.circular(
-                      10), // Optional: Adds rounded corners
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(
-                          0.5), // Shadow color with some transparency
-                      spreadRadius: 2,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // Changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Aadhar Card Number',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                    const SizedBox(height: 10),
-                    _imageField('Aadhar Card', aadharCardImage),
-                  ],
-                ),
-              ),
+              _buildDetailsContainer('Aadhar Card Number', null, null,
+                  'Aadhar Card', aadharCardImage),
               const SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.all(10), // Adds horizontal margin
-                decoration: BoxDecoration(
-                  color: Colors.white, // Set the background color
-                  borderRadius: BorderRadius.circular(
-                      10), // Optional: Adds rounded corners
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(
-                          0.5), // Shadow color with some transparency
-                      spreadRadius: 2,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // Changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'PAN Card Number',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.text,
-                    ),
-                    const SizedBox(height: 10),
-                    _imageField('PAN Card', panCardImage),
-                  ],
-                ),
-              ),
+              _buildDetailsContainer(
+                  'PAN Card Number', null, null, 'PAN Card', panCardImage),
               const SizedBox(height: 10),
             ],
           ),
@@ -149,12 +91,77 @@ class _DetailsPageState extends State<DetailsPage> {
     );
   }
 
+  Widget _buildDetailsContainer(String firstField, String? secondField,
+      String? buttonTitle, String imageFieldTitle, XFile? imageFile,
+      {bool cameraOnly = false}) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: isEnglish ? firstField : translateToHindi(firstField),
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          if (secondField != null) const SizedBox(height: 10),
+          if (secondField != null)
+            TextFormField(
+              decoration: InputDecoration(
+                labelText:
+                    isEnglish ? secondField : translateToHindi(secondField),
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          if (buttonTitle != null) const SizedBox(height: 10),
+          if (buttonTitle != null)
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurpleAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                isEnglish ? buttonTitle : translateToHindi(buttonTitle),
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+            ),
+          const SizedBox(height: 10),
+          _imageField(imageFieldTitle, imageFile, cameraOnly: cameraOnly),
+        ],
+      ),
+    );
+  }
+
   Widget _imageField(String title, XFile? imageFile,
       {bool cameraOnly = false}) {
+    String translatedTitle = isEnglish ? title : translateToHindi(title);
+    String buttonTitle = cameraOnly ? 'Click a Selfie' : 'Upload Image';
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        Text(
+          translatedTitle,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
         const SizedBox(height: 5),
         imageFile != null
             ? Image.file(File(imageFile.path))
@@ -175,8 +182,8 @@ class _DetailsPageState extends State<DetailsPage> {
               cameraOnly ? ImageSource.camera : ImageSource.gallery,
               title.toLowerCase()),
           child: Text(
-            'Pick ${cameraOnly ? "from Camera" : "Image"}',
-            style: TextStyle(
+            isEnglish ? buttonTitle : translateToHindi(buttonTitle),
+            style: const TextStyle(
                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
           ),
         ),
