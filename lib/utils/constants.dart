1df5f00e56b1bc4cd0213002b47e904eb99dd104
girithc <girithmchoudhary@gmail.com>
2024-01-22 19:28:38 +0530

    baseUrl update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,4 +3,4 @@ const String baseUrl = prodUrl;
 const String modApikey = "AIzaSyARoMzt-tHvh--HVMyRArfxEsoFnZR9VtQ";
 
 const String devUrl = "http://localhost:8080";
-const String prodUrl = "https://pronto-go-3ogvsx3vlq-el.a.run.app";
+const String prodUrl = "https://otto-mart-2cta4tgbnq-el.a.run.app";
